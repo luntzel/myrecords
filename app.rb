1e660a2c657df359b013c91ddc2d232ee652af0a
Mark Luntzel <mark@luntzel.com>
2013-11-14 11:50:09 -0800

    typo

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ configure do
   conn = MongoClient.new("localhost", 27017)
   set :mongo_connection, conn
   set :mongo_db, conn.db('myrecords')
-en
+end
 
 # The app
 class Testing < Sinatra::Base
