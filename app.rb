a0aeacaefddf16b96411eb0392bfdc07d9c6713c
Mark Luntzel <mark@luntzel.com>
2013-11-14 16:51:55 -0800

    lets simplify things

--- app.rb
+++ app.rb
@@ -3,12 +3,10 @@ require "rubygems"
 require "bundler/setup"
 require 'sinatra'
 require 'mongo'
-require 'json/ext' # required for .to_json
 
 
 configure do
   set :logging, true
-  db = Mongo::Connection.new.db('myrecords');
 end
 
 # The app
@@ -16,9 +14,4 @@ class Myrecords < Sinatra::Base
   get "/" do
     "Hello, is it me you're looking for?"
   end
-
-  get '/bands/?' do
-    content_type :json
-  end
-
 end
