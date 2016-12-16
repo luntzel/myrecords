945ba953fbcbba96992fe3bd54e865db2dbfa8d4
Mark Luntzel <mluntzel@nasinsurance.com>
2016-12-15 16:18:29 -0800

    testing to get rid of deprecation warning

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ role :app, "54.183.118.154"
 
 
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
+#after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
